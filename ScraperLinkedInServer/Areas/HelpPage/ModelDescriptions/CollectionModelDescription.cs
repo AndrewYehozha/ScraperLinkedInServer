e2729e05a51019971d3e245bc8626ca0933f9664
Yehozha Andrew <andrew.yehozha@gmail.com>
2020-01-22 01:08:05 +0200

    Task_1 Create project and add token manager

---
+++ ScraperLinkedInServer/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ScraperLinkedInServer.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
